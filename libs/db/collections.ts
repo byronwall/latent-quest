bedcd4dac561106b1558eb2aed0aabf4ed7f9e7c
Byron Wall <byron@byroni.us>
2022-12-04 00:05:21 -0500

    Resolve db foreign key delete cascade

--- libs/db/collections.ts
+++ libs/db/collections.ts
@@ -2,6 +2,7 @@ import { supabase } from "./supabase";
 
 import { getUuid } from "../shared-types/src";
 
+import type { SdImage } from "../shared-types/src";
 import type { LqCollection, LqDbCollection } from "../../model/collections";
 
 export async function db_upsertCollection(collection: LqDbCollection) {
@@ -20,18 +21,27 @@ export async function db_upsertCollection(collection: LqDbCollection) {
 }
 
 export async function db_getCollection(collectionId: string) {
-  // the images(*) relies on a composite primary key
+  // this uses the join table and rebuilds the object later
   const { data, error } = await supabase
-    .from("collections")
-    .select("*, images(*)")
-    .eq("id", collectionId)
-    .single();
+    .from("collection_images")
+    .select("images(*), collections(*)")
+    .eq("collectionId", collectionId);
 
   if (error) {
     throw error;
   }
 
-  const result = data as LqCollection;
+  type result = {
+    images: SdImage;
+    collections: LqDbCollection;
+  };
+
+  const _result = data as result[];
+
+  const result: LqCollection = {
+    ..._result[0].collections,
+    images: _result.map((r) => r.images),
+  };
 
   // need to parse the JSONB fields since they're returned as strings
   result.images.forEach((image) => {
