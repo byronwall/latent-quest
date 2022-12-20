120442c87f5348ab0b9e9b32c4aeac2a0b1dfd33
Byron Wall <byron@byroni.us>
2022-12-19 22:31:35 -0500

    Add some styles to the create new image page

--- components/ImageList.tsx
+++ components/ImageList.tsx
@@ -43,7 +43,7 @@ export function ImageList(props: ImageListProps) {
   return (
     <div>
       <div>
-        <div className="grid grid-cols-3  md:grid-cols-4 lg:grid-cols-5 gap-2">
+        <div className="grid grid-cols-2 sm:grid-cols-3  md:grid-cols-4 lg:grid-cols-5 gap-2">
           <div className="p-3 flex flex-col gap-2">
             <h1 className="text-3xl">image groups</h1>
             <p>
--- pages/create.tsx
+++ pages/create.tsx
@@ -1,10 +1,15 @@
-import { Title } from "@mantine/core";
 import { SdNewImagePrompt } from "../components/SdNewImagePrompt";
 
 export default function CreatePrompt() {
   return (
-    <div className="container">
-      <Title order={1}>test a prompt</Title>
+    <div className="mx-auto mt-4 flex max-w-3xl gap-4">
+      <div className="flex w-40 shrink-0 flex-col gap-2">
+        <h1 className="text-3xl"> new image</h1>
+        <p>
+          Use the controls to create a new image. This will also create a new
+          group to hold the results.
+        </p>
+      </div>
       <SdNewImagePrompt />
     </div>
   );
